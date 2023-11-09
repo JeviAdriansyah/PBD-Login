68f61a18b992a69efc98d08d298f7b8a04aafa1b
jvadrn <jeviadriansyah25@gmail.com>
2023-11-09 15:47:11 +0700

    comit

--- belajar-php-login/app/Config/Database.php
+++ belajar-php-login/app/Config/Database.php
@@ -6,10 +6,10 @@ class Database
 {
     private static ?\PDO $pdo = null;
 
-    public static function getCOnnection(string $env = "test"): \PDO{
-        if (self::$pdo == null){
-            //create new PDO
-            require_once  __DIR__ .'/../../config/database.php';
+    public static function getConnection(string $env = "test"): \PDO{
+        if(self::$pdo == null){
+            // create new PDO
+            require_once __DIR__ . '/../../config/database.php';
             $config = getDatabaseConfig();
             self::$pdo = new \PDO(
                 $config['database'][$env]['url'],
@@ -19,5 +19,17 @@ class Database
         }
 
         return self::$pdo;
+    }
+
+    public static function beginTransaction(){
+        self::$pdo->beginTransaction();
+    }
+
+    public static function commitTransaction(){
+        self::$pdo->commit();
+    }
+
+    public static function rollbackTransaction(){
+        self::$pdo->rollBack();
     }
 }
---
+++ belajar-php-login/app/Respository/SessionRepositoryTest.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace ProgrammerZamanNow\Belajar\PHP\MVC\Respository;
+
+class SessionRepositoryTest
+{
+
+}
